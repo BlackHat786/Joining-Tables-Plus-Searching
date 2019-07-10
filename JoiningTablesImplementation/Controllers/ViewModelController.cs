e6d1005819ce325fbb3f5f6beb7f4d3b252b3f55
Mohammed Ameen Abdool <21612147@dut4life.ac.za>
2019-07-10 15:09:26 +0200

    Comments

--- JoiningTablesImplementation/Controllers/ViewModelController.cs
+++ JoiningTablesImplementation/Controllers/ViewModelController.cs
@@ -13,11 +13,11 @@ namespace JoiningTablesImplementation.Controllers
         // GET: ViewModel
         public ActionResult Index(string search)
         {
-            List<ViewModel> vm = new List<ViewModel>();
+            List<ViewModel> vm = new List<ViewModel>();//List Of the View Model
             var list = (from x in db.Courses
                         join a in db.Students on x.CourseId equals a.CourseId
-                        select new { a.StdName, a.stdNum, x.CourseName, x.StartDate, x.EndDate });
-            foreach (var item in list)
+                        select new { a.StdName, a.stdNum, x.CourseName, x.StartDate, x.EndDate });//Linq Statement Joining the Tables on the Foreign Key
+            foreach (var item in list)//Compare the List To the model
             {
                 ViewModel vms = new ViewModel();
                 vms.StdName = item.StdName;
@@ -26,12 +26,12 @@ namespace JoiningTablesImplementation.Controllers
                 vms.StartDate = item.StartDate;
                 vms.EndDate = item.EndDate;
 
-                vm.Add(vms);
+                vm.Add(vms);//Add the Compared values to the list
              
             }
 
             
-            return View(vm.ToList());
+            return View(vm.ToList());//Return the list
         }
 
        
